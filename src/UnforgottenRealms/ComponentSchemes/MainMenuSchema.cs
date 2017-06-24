5fe240bb80e6b9dde9b21c464a30bb9005d42333
lszafirski <lukasz.szafirski@wp.pl>
2017-06-24 12:50:05 +0200

    Created schema for navigation buttons

---
+++ src/UnforgottenRealms/ComponentSchemes/MainMenuSchema.cs
@@ -0,0 +1,45 @@
+﻿using SFML.Graphics;
+using SFML.System;
+using UnforgottenRealms.Core.Utils;
+using UnforgottenRealms.UI.Components.Rectangle;
+
+namespace UnforgottenRealms.ComponentSchemes
+{
+    public class MainMenuSchema
+    {
+        public Color HighlightedComponentColor { get; set; } = Color.Red;
+        public Color IdleComponentColor { get; set; } = Color.Black;
+        public uint FontSize { get; set; } = 24;
+        public Color TextColor { get; set; } = Color.White;
+
+        public int NavigationButtonHeight { get; set; }
+        public Vector2f NavigationButtonSize { get; set; } = new Vector2f(128, 40);
+
+        public Button CreateNavigationButton(int heightOrder, string text)
+        {
+            var button = new Button();
+            SetBaseValues(button);
+            button.Text.DisplayedString = text;
+            button.HighlightColor = HighlightedComponentColor;
+            button.IdleColor = IdleComponentColor;
+            button.Position = new Vector2f(0, heightOrder * NavigationButtonSize.Y + NavigationButtonHeight);
+            return button;
+        }
+
+        private void SetBaseValues<T>(T component) where T : RectangleComponentBase
+        {
+            component.Shape = new RectangleShape
+            {
+                FillColor = IdleComponentColor,
+                Size = NavigationButtonSize
+            };
+            component.Text = new Text
+            {
+                Font = FontExtensions.Arial,
+                FillColor = TextColor,
+                CharacterSize = FontSize
+            };
+            component.TextPosition = new Vector2f(4, 4);
+        }
+    }
+}
